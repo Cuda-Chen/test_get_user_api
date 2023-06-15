10a427d9a7c27c4c60cfb2783f957405692dbe22
Cuda-Chen <clh960524@gmail.com>
2023-06-15 14:40:54 +0800

    Finish function

--- get_user_api.py
+++ get_user_api.py
@@ -1,6 +1,13 @@
 import urllib.request
 import json
 
+foo = """
+{"page":1,"per_page":3,"total":12,"total_pages":4,"data":
+    [{"id":1,"first_name":"George","last_name":"Bluth","avatar":"https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg"},
+    {"id":2,"first_name":"Janet","last_name":"Weaver","avatar":"https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg"},
+    {"id":3,"first_name":"Emma","last_name":"Wong","avatar":"https://s3.amazonaws.com/uifaces/faces/twitter/olegpogodaev/128.jpg"}]}
+"""
+
 HEADER_USER_AGENT = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.79 Safari/537.36'
 }
@@ -27,8 +34,22 @@ def request_users(users, page_number=1):
         for d in data_json['data']:
             users[d['id']] = f"{d['first_name']} {d['last_name']}"
 
-def get_user_full_name_list(start, end):
-    pass
+# 1-index
+def get_user_full_name_list(start, end, users):
+    ans_lists = []
+
+    if not isinstance(start, int) or not isinstance(end, int):
+        return ans_lists
+
+    sz = len(users)
+    if start < 1 or end < start or end > sz:
+        return ans_lists
+
+    for i in range(start, end + 1):
+        ans_lists.append(users[i])
+
+    ans_lists.sort()
+    return ans_lists
 
 if __name__ == "__main__":
     users = {} # {id, full_name}
@@ -37,4 +58,7 @@ if __name__ == "__main__":
         request_users(users, i)
     print(users)
 
-
+    l = get_user_full_name_list(5, 8, users)
+    print(l)
+    assert get_user_full_name_list (1, 3, users) == ['Emma Wong', 'George Bluth', 'Janet Weaver']
+    assert get_user_full_name_list (5, 8, users) == ['Charles Morris', 'Emma Wong', 'Eve Holt', 'Janet Weaver']
